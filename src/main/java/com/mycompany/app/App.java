adbe4c78108e71760f34f120502ed8c1b25e8c04
karthi150515 <115717602+karthi150515@users.noreply.github.com>
2022-11-04 14:50:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello Tn$dc@!" );
         System.out.println( "Hello K@RTI" );
         System.out.println( "Hello @RTI" );
+        System.out.println( "Hello @" );
     }
 }
